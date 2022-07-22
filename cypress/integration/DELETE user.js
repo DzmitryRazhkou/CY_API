62ae3443126e68e2bd6717ad2f35b90b934d0794
dzmitryrazhkou <dzmitryrazhkou@gmail.com>
2022-07-22 14:13:15 -0700

    d

--- cypress/integration/DELETE user.js
+++ cypress/integration/DELETE user.js
@@ -1,52 +1,50 @@
-describe('POST call', () => {
+describe("POST call", () => {
+  let token =
+    "44656c4fb83ebdac04cfdc8e5e97cb69b567347dbc40c982a1c350dee2efa0ec";
 
-    let token = "44656c4fb83ebdac04cfdc8e5e97cb69b567347dbc40c982a1c350dee2efa0ec"
-   
-    // CREATE AN USER
+  // CREATE AN USER
 
-        it.only('Post user', () => {
-    
-            cy.request({
-                method: 'POST',
-                url: 'https://gorest.co.in/public/v1/users',
-                headers: {
-                    'Authorization': 'Bearer ' + token
-                },
-                body: {
-                        "name": "CY_CreateForDelete",
-                        "email": "cypress_createForDelete125@com",
-                        "gender": "male",
-                        "status": "active"
-                }
-    
-            }).then((res) => {
-                cy.log(JSON.stringify(res))
-                expect(res.status).to.eq(201)
-                expect(res.body.meta).to.be.null
-                expect(res.body.data).has.property('name', "CY_CreateForDelete")
-                expect(res.body.data).has.property('email', "cypress_createForDelete125@com") 
-                expect(res.body.data).has.property('gender', "male") 
-                expect(res.body.data).has.property('status', "active") 
-            }).then((res) => {
-    
-                const userId = res.body.data.id
-                cy.log('User id is: ' +userId)
-    
-    // DELETE AN USER
-
-                cy.request({
-                    method: 'DELETE',
-                    url: 'https://gorest.co.in/public/v1/users/' +userId,
-                headers: {
-                    'Authorization': 'Bearer ' + token
-                },
-                 
-                }).then((res) => {
-                    expect(res.status).to.eq(204)
-
-                })
-    
-               })
-           })
-    
+  it.only("Post user", () => {
+    cy.request({
+      method: "POST",
+      url: "https://gorest.co.in/public/v1/users",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+      body: {
+        name: "CY_CreateForDelete",
+        email: "cypress_createForDelete_125@com",
+        gender: "male",
+        status: "active",
+      },
     })
+      .then((res) => {
+        cy.log(JSON.stringify(res));
+        expect(res.status).to.eq(201);
+        expect(res.body.meta).to.be.null;
+        expect(res.body.data).has.property("name", "CY_CreateForDelete");
+        expect(res.body.data).has.property(
+          "email",
+          "cypress_createForDelete125_@com"
+        );
+        expect(res.body.data).has.property("gender", "male");
+        expect(res.body.data).has.property("status", "active");
+      })
+      .then((res) => {
+        const userId = res.body.data.id;
+        cy.log("User id is: " + userId);
+
+        // DELETE AN USER
+
+        cy.request({
+          method: "DELETE",
+          url: "https://gorest.co.in/public/v1/users/" + userId,
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }).then((res) => {
+          expect(res.status).to.eq(204);
+        });
+      });
+  });
+});
--- cypress/integration/PUT user.js
+++ cypress/integration/PUT user.js
@@ -1,54 +1,53 @@
-describe('POST call', () => {
+describe("POST call", () => {
+  let token =
+    "44656c4fb83ebdac04cfdc8e5e97cb69b567347dbc40c982a1c350dee2efa0ec";
 
-let token = "44656c4fb83ebdac04cfdc8e5e97cb69b567347dbc40c982a1c350dee2efa0ec"
-
-    it.only('Post user', () => {
+  it.only("Post user", () => {
+    cy.request({
+      method: "POST",
+      url: "https://gorest.co.in/public/v1/users",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+      body: {
+        name: "CY_Test",
+        email: "cypress_test125@com",
+        gender: "male",
+        status: "active",
+      },
+    })
+      .then((res) => {
+        cy.log(JSON.stringify(res));
+        expect(res.status).to.eq(201);
+        expect(res.body.meta).to.be.null;
+        expect(res.body.data).has.property("name", "CY_Test");
+        expect(res.body.data).has.property("email", "cypress_test125@com");
+        expect(res.body.data).has.property("gender", "male");
+        expect(res.body.data).has.property("status", "active");
+      })
+      .then((res) => {
+        const userId = res.body.data.id;
+        cy.log("User id is: " + userId);
 
         cy.request({
-            method: 'POST',
-            url: 'https://gorest.co.in/public/v1/users',
-            headers: {
-                'Authorization': 'Bearer ' + token
-            },
-            body: {
-                    "name": "CY_Test",
-                    "email": "cypress_test125@com",
-                    "gender": "male",
-                    "status": "active"
-            }
-
+          method: "PUT",
+          url: "https://gorest.co.in/public/v1/users/" + userId,
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+          body: {
+            name: "CY_Test_Updated",
+            email: "cypress_test_updated125@com",
+          },
         }).then((res) => {
-            cy.log(JSON.stringify(res))
-            expect(res.status).to.eq(201)
-            expect(res.body.meta).to.be.null
-            expect(res.body.data).has.property('name', "CY_Test")
-            expect(res.body.data).has.property('email', "cypress_test125@com") 
-            expect(res.body.data).has.property('gender', "male") 
-            expect(res.body.data).has.property('status', "active") 
-        }).then((res) => {
-
-            const userId = res.body.data.id
-            cy.log('User id is: ' +userId)
-
-            cy.request({
-                method: 'PUT',
-                url: 'https://gorest.co.in/public/v1/users/' +userId,
-            headers: {
-                'Authorization': 'Bearer ' + token
-            },
-            body: {
-                "name": "CY_Test_Updated",
-                "email": "cypress_test_updated125@com",
-             }
-
-            }).then((res) => {
-                expect(res.status).to.eq(200)
-                expect(res.body.data).has.property('id', userId)
-                expect(res.body.data).has.property('name', "CY_Test_Updated")
-                expect(res.body.data).has.property('email', "cypress_test_updated125@com") 
-            })
-
-           })
-       })
-
-})
+          expect(res.status).to.eq(200);
+          expect(res.body.data).has.property("id", userId);
+          expect(res.body.data).has.property("name", "CY_Test_Updated");
+          expect(res.body.data).has.property(
+            "email",
+            "cypress_test_updated125@com"
+          );
+        });
+      });
+  });
+});
